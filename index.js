646b413fa88ffb9a77cff69de92d9e6e9f310d9c
orrbarkat <barkat.orr@gmail.com>
2023-04-01 16:58:35 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const { Client, LocalAuth } = require("whatsapp-web.js");
+
+const client = new Client({
+  authStrategy: new LocalAuth(),
+  puppeteer: { headless: false },
+});
+
+client.on("loading_screen", (percent, message) => {
+  console.log("LOADING SCREEN", percent, message);
+});
+
+client.on("qr", (qr) => {
+  // NOTE: This event will not be fired if a session is specified.
+  console.log("QR RECEIVED", qr);
+});
+
+client.on("authenticated", () => {
+  console.log("AUTHENTICATED");
+});
+
+client.on("auth_failure", (msg) => {
+  // Fired if session restore was unsuccessful
+  console.error("AUTHENTICATION FAILURE", msg);
+});
+
+client.on("ready", () => {
+  console.log("READY");
+});
+
+client.initialize();
