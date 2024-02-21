9993adc81de0fb0229aefbb4949cd73b06592a6c
orrbarkat <barkat.orr@gmail.com>
2024-02-21 09:01:31 +0000

    Integrate face-api

--- src/image_filter.js
+++ src/image_filter.js
@@ -1,10 +1,97 @@
 import AWS from 'aws-sdk';
+import * as faceapi from 'face-api.js';
+
+export const FILTER_MODE_LOCAL = 'LOCAL';
+export const FILTER_MODE_AWS = 'AWS';
+
 
 class ImageFilter {
+  constructor(referenceImagePaths, filterMode) {
+    switch (filterMode) {
+      case FILTER_MODE_LOCAL:
+        this._filter = new LocalImageFilter(referenceImagePaths);
+        break;
+      case FILTER_MODE_AWS:
+        this._filter = new AwsImageFilter(referenceImagePaths);
+        break;
+      default:
+        throw new Error(`Invalid filter mode: ${filterMode}`);
+    }
+  }
+
+  async filter(media) {
+    return this._filter.filter(media);
+  }
+
+  static async createFilter(referenceImagePaths, filterMode) {
+    const filter = new ImageFilter(referenceImagePaths, filterMode);
+    await filter._filter.init();
+    return filter;
+  }
+}
+
+// A class to leverage face-api.js for image filtering based on https://github.com/computervisioneng/face-recognition-javascript-webcam-faceapi/blob/main/script.js
+class LocalImageFilter {
+  constructor(referenceImagePaths) {
+    this.referenceImagePaths = referenceImagePaths;
+  }
+
+  init() {
+    return Promise.all([
+      // load models
+      faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+      faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+      faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
+      // initialize LabeledFaceDescriptors based on referenceImagePaths
+      this.createFaceMatcher().then(faceMatcher => this.faceMatcher = faceMatcher)
+    ]);
+  }
+
+  async createFaceMatcher() {
+    const faceDescriptors = await Promise.all(
+      this.referenceImagePaths.map(async (path) => faceapi.fetchImage(path).then(img => faceapi.detectAllFaces(img).withFaceLandmarks().withFaceDescriptors())
+      ));
+    return new faceapi.FaceMatcher(faceDescriptors);
+  }
+
+  async filter(media) {
+    if (!ImageFilter.validateImage(media)) {
+      console.log(`Invalid image type, got: ${media.mimetype}`);
+      return false;
+    }
+    if (!this.hasFaces(media.data)) {
+      console.log(`No faces found in image`);
+      return false;
+    }
+    const img = await faceapi.bufferToImage(media.data);
+    const detections = await faceapi.detectAllFaces(img).withFaceLandmarks().withFaceDescriptors();
+    const matchResults = detections.map(detection => this.faceMatcher.findBestMatch(detection.descriptor));
+    return matchResults.some(matches => matches.some(match => match._label !== 'unknown'));
+  }
+
+  static validateImage(media) {
+    return media.mimetype === 'image/jpeg' || media.mimetype === 'image/png';
+  }
+
+  async hasFaces(image) {
+    const img = await faceapi.bufferToImage(image);
+    const detections = await faceapi.detectAllFaces(img);
+    return detections.length > 0;
+  }
+}
+
+/**
+ * Represents an AWS image filter.
+ * @class
+ */
+class AwsImageFilter {
+
   constructor(referenceImagePaths) {
     this._rekognition = new AWS.Rekognition();
     this.referenceImagePaths = referenceImagePaths;
   }
+
+  init() {}
 
   async filter(media) {
     if (!ImageFilter.validateImage(media)) {
--- src/try_upload.js
+++ src/try_upload.js
@@ -1,11 +1,12 @@
 import AWS from 'aws-sdk';
 import Saver, { MODE_AWS } from "./saver.js";
-import ImageFilter from "./image_filter.js";
+import ImageFilter, { FILTER_MODE_LOCAL } from "./image_filter.js";
 import pkg from 'whatsapp-web.js';
 const { MessageMedia } = pkg;
 
 const config = {
   storageMode: MODE_AWS,
+  filterMode: FILTER_MODE_LOCAL,
   refrenceImages: [
     "s3://whatsapp-photos/7e0bac58-87f6-4b64-b603-77efb2766bc7.jpg",
     "s3://whatsapp-photos/IMG_20160502_090658.jpg",
@@ -20,7 +21,7 @@ AWS.config.update({
   region: process.env.AWS_DEFAULT_REGION
 });
 
-const imageFilter = new ImageFilter(config.refrenceImages);
+const imageFilter = new ImageFilter(config.refrenceImages, config.filterMode);
 const saver = new Saver(config.storageMode);
 
 const media = MessageMedia.fromFilePath("./images/IMG_7024.jpg");
